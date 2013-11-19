878e5036c9e61ecf32dc3453d17f78d273a7a2da
Steven Burgess <sburgess@dattobackup.com>
2013-11-19 18:17:57 -0500

    more work on the finder
    This adds the command_count function that will allow for a more robust
    way of telling if a call is a real call or a false positive

--- finder.py
+++ finder.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 #This script will discover how many times each command specified in
 #comands.cfg are used in the test suite
@@ -13,20 +14,48 @@ for line in commands_file:
 		#the lines are formated
 		#export NAME="path"
 		#so I want the part beteinxt the space and the =
-		command_name = '$' + line[line.index(' ')+1:line.index('=')]
+		command_name = line[line.index(' ')+1:line.index('=')]
+		#when looking at each line, you are looking for the command
+		#it will have $ before it, and either ' ', '{', or '}' after
+		#it
+		command_name = '$' + command_name
 		command_dict[command_name] = 0
 
 print command_dict
 
+character_dict = dict()
+
+#return 1 if this command is in the line, 0 otherwise
+def command_count(command, line):
+	#get the character directly after the command to see that it is
+	#ACTUALLY a call, and no just a fake
+	index_of_command = line.index(command)
+	length_of_command = len(command)
+	location_of_command_end = index_of_command + length_of_command
+	character_after_command = line[location_of_command_end:location_of_command_end+1]
+	#we think that if there is a space, an open curly bracket, or a close
+	#curly bracket, then this is a valid calls
+	if character_after_command in ' {}':
+		return 1
+
+	#if the character directly after the command is a letter, then it is
+	#not a call
+	if character_after_command in string.ascii_letters:
+		return 0
+	#now we need to look at these
+	#print character_after_command + '|'
+	return 0
+
+
 #now walk the enitre ZFS project tree and find refernces
 for root, dirs, files in os.walk('usr/src/test/zfs-tests'):
 	for file_name in files:
-		print "looking at" + file_name
 		file_path = os.path.join(root, file_name)
 		project_file = open(file_path)
 		for line in project_file:
 			for command in command_dict:
 				if command in line:
-					command_dict[command] = command_dict[command] + 1
+					count = command_count(command, line)
+					command_dict[command] = command_dict[command] + count
 
 print command_dict
